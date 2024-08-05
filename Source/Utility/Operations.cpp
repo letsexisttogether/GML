461c64b72ca6109896d86b2510098f238fa2a7bf
letsexisttogether <letmeexist95@gmail.com>
2024-08-05 16:10:12 +0300

    Implemented basic operations of convertion rad to deg and deg to rad

---
+++ Source/Utility/Operations.cpp
@@ -0,0 +1,13 @@
+#include "Operations.hpp"
+
+#include "Variables.hpp"
+
+Angle ConvertToRadians(const Angle degrees) noexcept
+{
+    return degrees * PI / 180.0f;
+}
+
+Angle ConvertToDegrees(const Angle radians) noexcept
+{
+    return radians * 180.0f / PI;
+}
---
+++ Source/Utility/Operations.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "Naming.hpp"
+
+Angle ConvertToRadians(const Angle degree) noexcept;
+Angle ConvertToDegrees(const Angle radians) noexcept;

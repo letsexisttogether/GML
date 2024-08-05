dfbf8a72f1d3925961efb3579465a1bb49bcf2c8
letsexisttogether <letmeexist95@gmail.com>
2024-08-06 00:19:41 +0300

    Saved the current version of the library

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,24 +1,16 @@
 #include <iostream>
 
 #include "Matrix/MatrixDefenitions.hpp"
-#include "Transformation/Transformation.hpp"
+#include "Variations/Transformation.hpp"
 #include "Vector/VectorDefinitions.hpp"
 
 std::int32_t main()
 {
     std::cout << "Hello, GML" << std::endl;
 
-    Vec3f vec{ 5.0f, 4.0f, 3.0f };
+    Vec3f vec{ 1.0f, 0.8f, 0.0f };
 
     std::cout << vec.X() << ' ' << vec.Y() << ' ' << vec.Z() << '\n';
-
-
-    Vec3f normalizedVec{ vec.GetNormalized() };
-
-    std::cout << normalizedVec.X() << ' ' << normalizedVec.Y() 
-        << ' ' << normalizedVec.Z() << '\n';
-
-    std::cout << normalizedVec.GetLength() << std::endl;
 
     return EXIT_SUCCESS;
 }
--- Source/Utility/Operations.hpp
+++ Source/Utility/Operations.hpp
@@ -3,4 +3,5 @@
 #include "Naming.hpp"
 
 Angle ConvertToRadians(const Angle degree) noexcept;
+
 Angle ConvertToDegrees(const Angle radians) noexcept;

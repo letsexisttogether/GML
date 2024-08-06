c12d8d9d14357b81c75aa3156df25c1c7c2fa2bd
letsexisttogether <letmeexist95@gmail.com>
2024-08-06 10:53:37 +0300

    Fixed GetRotation - it now does not change the angle sing and does normalize the rotVec

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,16 +1,78 @@
+#include <ios>
 #include <iostream>
 
+#include <glm/gtc/matrix_transform.hpp>
+
 #include "Matrix/MatrixDefenitions.hpp"
+#include "Utility/Operations.hpp"
+#include "Variations/Projection.hpp"
 #include "Variations/Transformation.hpp"
 #include "Vector/VectorDefinitions.hpp"
+#include "glm/ext/matrix_clip_space.hpp"
+#include "glm/ext/matrix_transform.hpp"
+#include "glm/trigonometric.hpp"
+
+template <class _Matrix>
+void PrintMatrix(const _Matrix& matrix) noexcept;
+
+template <class _FMatrix, class _SMatrix>
+bool CompareMatrices(const _FMatrix& firstMatrix,
+    const _SMatrix secondMatrix) noexcept;
 
 std::int32_t main()
 {
     std::cout << "Hello, GML" << std::endl;
 
-    Vec3f vec{ 1.0f, 0.8f, 0.0f };
+    const Mat4x4f myMatrix{ GetRotation(ConvertToRadians(55.0f),
+        Vec3f{ 0.0f, 0.5f, 1.0f }) };
 
-    std::cout << vec.X() << ' ' << vec.Y() << ' ' << vec.Z() << '\n';
+    const glm::mat4 libMatrix{ glm::rotate(glm::mat4{ 1.0f },
+        glm::radians(55.0f), glm::vec3{ 0.0f, 0.5f, 1.0f }) };
+
+    PrintMatrix(myMatrix);
+    PrintMatrix(libMatrix);
+
+    std::cout << std::boolalpha << CompareMatrices(myMatrix, libMatrix);
+
+    std::cout << std::endl;
+
 
     return EXIT_SUCCESS;
+}
+
+template <class _Matrix>
+void PrintMatrix(const _Matrix& matrix) noexcept
+{
+    for (std::size_t i = 0; i < 4; ++i)
+    {
+        for (std::size_t j = 0; j < 4; ++j)
+        {
+            std::cout << matrix[i][j] << ' ';
+        }
+        std::cout << '\n';
+    }
+    std::cout << std::endl;
+}
+
+template <class _FMatrix, class _SMatrix>
+bool CompareMatrices(const _FMatrix& firstMatrix,
+    const _SMatrix secondMatrix) noexcept
+{
+    for (std::size_t i = 0; i < 4; ++i)
+    {
+        for (std::size_t j = 0; j < 4; ++j)
+        {
+            if (firstMatrix[i][j] != secondMatrix[i][j])
+            {
+
+                std::cout << "They differ at " << i << ' ' << j << '\n'
+                    << "The values are: " << firstMatrix[i][j] 
+                    << ' ' << secondMatrix[i][j] << std::endl;
+
+                return false;
+            }
+        }
+    }
+
+    return true;
 }
--- Source/Variations/Tranformation.cpp
+++ Source/Variations/Tranformation.cpp
@@ -31,37 +31,37 @@ Mat4x4f GetRotation(const Angle angle, const Vec3f& rotAxis)
 {
     Mat4x4f matrix{};
 
-    const float theta = -angle;
-
-    const float cos = std::cos(theta);
-    const float sin = std::sin(theta);
+    const float cos = std::cos(angle);
+    const float sin = std::sin(angle);
 
     const float cosDiff = 1.0f - cos;
 
-    const float xyCosDiff = rotAxis.X() * rotAxis.Y() * cosDiff;
-    const float xzCosDiff = rotAxis.X() * rotAxis.Z() * cosDiff;
-    const float yzCosDiff = rotAxis.Y() * rotAxis.Z() * cosDiff;
+    const Vec3f normalAxis{ std::move(rotAxis.GetNormalized()) };
 
-    const float xSin = rotAxis.X() * sin;
-    const float ySin = rotAxis.Y() * sin;
-    const float zSin = rotAxis.Z() * sin;
+    const float xyCosDiff = normalAxis.X() * normalAxis.Y() * cosDiff;
+    const float xzCosDiff = normalAxis.X() * normalAxis.Z() * cosDiff;
+    const float yzCosDiff = normalAxis.Y() * normalAxis.Z() * cosDiff;
 
-    const float xPowCosDiff = rotAxis.X() * rotAxis.X() * cosDiff;
-    const float yPowCosDiff = rotAxis.Y() * rotAxis.Y() * cosDiff;
-    const float zPowCosDiff = rotAxis.Z() * rotAxis.Z() * cosDiff;
+    const float xSin = normalAxis.X() * sin;
+    const float ySin = normalAxis.Y() * sin;
+    const float zSin = normalAxis.Z() * sin;
+
+    const float xPowCosDiff = normalAxis.X() * normalAxis.X() * cosDiff;
+    const float yPowCosDiff = normalAxis.Y() * normalAxis.Y() * cosDiff;
+    const float zPowCosDiff = normalAxis.Z() * normalAxis.Z() * cosDiff;
 
     // We do hard code the values
 
     matrix[0][0] = cos + xPowCosDiff;
-    matrix[0][1] = xyCosDiff - zSin;
-    matrix[0][2] = xzCosDiff + ySin;
+    matrix[0][1] = xyCosDiff + zSin;
+    matrix[0][2] = xzCosDiff - ySin;
 
-    matrix[1][0] = xyCosDiff + zSin;
+    matrix[1][0] = xyCosDiff - zSin;
     matrix[1][1] = cos + yPowCosDiff;
-    matrix[1][2] = yzCosDiff - xSin;
+    matrix[1][2] = yzCosDiff + xSin;
 
-    matrix[2][0] = xzCosDiff - ySin;
-    matrix[2][1] = yzCosDiff + xSin;
+    matrix[2][0] = xzCosDiff + ySin;
+    matrix[2][1] = yzCosDiff - xSin;
     matrix[2][2] = cos + zPowCosDiff;
 
     matrix[3][3] = 1.0f;

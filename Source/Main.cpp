153fe6528eb2086df17d6b5bec1ff86f65d0b2e4
letsexisttogether <letmeexist95@gmail.com>
2024-08-07 23:58:17 +0300

    Saved the current version the library

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -2,37 +2,27 @@
 #include <iostream>
 
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/ext/matrix_clip_space.hpp>
 
 #include "Matrix/MatrixDefenitions.hpp"
 #include "Utility/Operations.hpp"
 #include "Variations/Projection.hpp"
 #include "Variations/Transformation.hpp"
 #include "Vector/VectorDefinitions.hpp"
-#include "glm/ext/matrix_clip_space.hpp"
-#include "glm/ext/matrix_transform.hpp"
-#include "glm/trigonometric.hpp"
+#include "glm/ext/vector_float3.hpp"
 
 template <class _Matrix>
 void PrintMatrix(const _Matrix& matrix) noexcept;
 
 template <class _FMatrix, class _SMatrix>
 bool CompareMatrices(const _FMatrix& firstMatrix,
-    const _SMatrix secondMatrix) noexcept;
+    const _SMatrix& secondMatrix) noexcept;
 
 std::int32_t main()
 {
     std::cout << "Hello, GML" << std::endl;
 
-    const Mat4x4f myMatrix{ GetRotation(ConvertToRadians(55.0f),
-        Vec3f{ 0.0f, 0.5f, 1.0f }) };
-
-    const glm::mat4 libMatrix{ glm::rotate(glm::mat4{ 1.0f },
-        glm::radians(55.0f), glm::vec3{ 0.0f, 0.5f, 1.0f }) };
-
-    PrintMatrix(myMatrix);
-    PrintMatrix(libMatrix);
-
-    std::cout << std::boolalpha << CompareMatrices(myMatrix, libMatrix);
+    const Mat4x4f matrix{};
 
     std::cout << std::endl;
 
@@ -56,7 +46,7 @@ void PrintMatrix(const _Matrix& matrix) noexcept
 
 template <class _FMatrix, class _SMatrix>
 bool CompareMatrices(const _FMatrix& firstMatrix,
-    const _SMatrix secondMatrix) noexcept
+    const _SMatrix& secondMatrix) noexcept
 {
     for (std::size_t i = 0; i < 4; ++i)
     {
